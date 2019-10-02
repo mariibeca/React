37ac439a13d0dc5c03f5efc21cc78c0a26504d33
Mariane_Carvalho <mariibeca2012@gmail.com>
2019-10-02 17:27:10 -0300

    Rendering the app

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React,{ Component } from 'react';
+import '../css/styles.css'
+
+//onclick vanilla JS
+
+
+
+class Header extends Component {
+
+    inputChangeHandler(event){
+        console.log(event.target.value)
+    }
+
+    render(){ 
+        return (
+            <header>
+                <div className="logo">Logo</div>
+                <input type="text" onChange={this.inputChangeHandler}/>
+            </header>
+        )
+    }
+}
+
+export default Header;
