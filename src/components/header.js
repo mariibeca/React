f0c2cc06484bc82c3845ef105c56e3d71a649f6f
Mariane_Carvalho <mariibeca2012@gmail.com>
2019-10-03 12:24:02 -0300

    Adding some style

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React,{ Component } from 'react';
+import '../css/styles.css'
 
 
 class Header extends Component {
     render () {
+       
         return ( 
             <header>
-                <div>Logo</div>
+                <div className="logo">Logo</div>
                 <input type="text"/>
             </header> 
         )
