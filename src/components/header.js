a8c5ee08bc82b7c500df04a37d7c1bfe5643362d
Mariane_Carvalho <mariibeca2012@gmail.com>
2019-10-08 11:41:45 -0300

    More on Styles

--- src/components/header.js
+++ src/components/header.js
@@ -4,18 +4,42 @@ import '../css/styles.css'
 
 class Header extends Component {
 
-    state = {        
+    state = {
+        //active:false,
+        //active:'active',
+                
         keywords:''
     }
 
     inputChangeHandler = (event) => {
+        //const value = event.target.value === '' ? false:true;
+        //const value = event.target.value === '' ? 'active' : 'non-active'
+
         this.setState({
+            //active:value,
+
             keywords: event.target.value
-        })
+        });
     }
 
     render () {
+
+        // const style = {
+        //     background:'red'
+        // }
+
+        // if(this.state.keywords !== ''){
+        //     style.background = 'blue'
+        // } else{
+        //     style.background = 'red'
+        // }
+
+
         return ( 
+            // inline option <header style={{background:`${this.state.active ? 'red':'blue'}`}}>
+
+            // class style option <header className={this.state.active}>
+
             <header>
                 <div className="logo">Logo</div>
                 <input type="text" onChange={this.inputChangeHandler}/>
