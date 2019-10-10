ee93ec2ca59cb6ac877957c4a5225d22481d9373
Mariane_Carvalho <mariibeca2012@gmail.com>
2019-10-10 13:30:20 -0300

    Using links in routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 //COMPONENTS
 
@@ -12,7 +12,12 @@ const App = () => {
     return (
          <BrowserRouter>
             <div>
-                <header>Header</header>
+                <header>
+                    <Link to="/">Home</Link><br/>
+                    <Link to="/posts">Posts</Link><br/>
+                    <Link to="/profile">Profile</Link><br/>
+                    <hr/>
+                </header>
                 <Route path="/" exact component={Home}/>
                 <Route path="/posts" component={Posts}/>
                 <Route path="/profile" component={Profile}/>         
