a83edfb937b660087bd5e159a954fa980f3d8f70
Mariane_Carvalho <mariibeca2012@gmail.com>
2019-10-21 22:34:15 -0300

    HOC

---
+++ src/hoc/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = (props) => {
+    const style = {
+        background:'lightgrey'
+    }
+    return (
+        <div style={style}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Card;
